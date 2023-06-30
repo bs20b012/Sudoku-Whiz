ea3e30a3823b5f700b7d76fd9e16cffaa4aba9cf
Boneshwar V K <103631495+bs20b012@users.noreply.github.com>
2023-06-30 11:28:49 +0530

    Update and rename NumberExtractor.py to Number_Extractor.py

--- Number_Extractor.py
+++ Number_Extractor.py
@@ -1,15 +1,17 @@
+# Importing Libraries
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import model_from_json
 
-# Load the saved model
+# Loading the saved model
 json_file = open('models/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
+
+# Loading weights into the new model
 loaded_model.load_weights("models/model.h5")
 print("Loaded saved model from disk.")
  
@@ -17,16 +19,12 @@ print("Loaded saved model from disk.")
 def identify_number(image):
     image_resize = cv2.resize(image, (28,28))    # For plt.imshow
     image_resize_2 = image_resize.reshape(1,1,28,28)    # For input to model.predict_classes
-#    cv2.imshow('number', image_test_1)
+
     loaded_model_pred = loaded_model.predict_classes(image_resize_2 , verbose = 0)
-#    print('Prediction of loaded_model: {}'.format(loaded_model_pred[0]))
-    return loaded_model_pred[0]
+ return loaded_model_pred[0]
 
 def extract_number(sudoku):
     sudoku = cv2.resize(sudoku, (450,450))
-#    cv2.imshow('sudoku', sudoku)
-
-    # split sudoku
     grid = np.zeros([9,9])
     for i in range(9):
         for j in range(9):
